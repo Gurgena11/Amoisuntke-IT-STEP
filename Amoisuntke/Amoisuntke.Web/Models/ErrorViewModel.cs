7d819c6b1a530c060162b78acd16a6983e9b31f3
საბა გურგენიძე <s.gurgenidze@itstep.ge>
2023-11-27 08:36:25 -0800

    Init

---
+++ Amoisuntke/Amoisuntke.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amoisuntke.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
